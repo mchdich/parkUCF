0c0052411d659e9b5450343ce1e2e43394df7547
Mehdi Chraibi <chraibimehdi11@gmail.com>
2025-12-22 19:33:07 -0500

    Updated frontend and backend schema

--- app/app/components/DynamicChart.tsx
+++ app/app/components/DynamicChart.tsx
@@ -1,29 +1,44 @@
 import Chart from 'chart.js/auto';
 import ChartDeferred from 'chartjs-plugin-deferred';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export default function DynamicChart (props: {type: string}) {
-    const canvasRef = useRef(null);
-    
-    useEffect(() => {
-        fetch('https://parkucf-hsbpefgpekhph5fv.centralus-01.azurewebsites.net/api/weekly')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error('Error fetching data', error));
-        
-        const ctx = canvasRef.current;
-        
-        const myChart = new Chart(ctx, {
-            plugins: [ChartDeferred],
-            type: 'line',
-            data: {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [data, setData] = useState<any[]>([]);
 
-            }
-        });
-        
+    useEffect(() => {
+        let myChart: Chart | null = null;
+        fetch(`https://parkucf-hsbpefgpekhph5fv.centralus-01.azurewebsites.net/api/${props.type}`)
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+                const ctx = canvasRef.current;
+                if (!ctx) return;
+                myChart = new Chart(ctx, {
+                    plugins: [ChartDeferred],
+                    type: 'line',
+                    data: {
+                        labels: data.map((x: any) => x.day),
+                        datasets: [
+                            ...['a','b','c','d','g','h','i'].map(letter => ({
+                                label: letter.toUpperCase(),
+                                data: data
+                                    .filter((row: any) => row.id[0] === letter)
+                                    .map((row: any) => ({
+                                        x: row.x,
+                                        y: row.y,
+                                    }))
+                            }))
+                        ]
+                    }
+                });
+            })
+            .catch(error => console.error('Error fetching data', error));
+
         return () => {
-            myChart.destroy()
+            if (myChart) myChart.destroy();
         };
     }, [props.type]);
+
     return <canvas ref={canvasRef}></canvas>
 }
