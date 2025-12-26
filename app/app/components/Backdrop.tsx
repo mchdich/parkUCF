366297c251df004942d07db93fa61a5006ae763e
Mehdi Chraibi <chraibimehdi11@gmail.com>
2025-12-25 20:28:19 -0500

    Readjusted mobile backdrop

--- app/app/components/Backdrop.tsx
+++ app/app/components/Backdrop.tsx
@@ -17,7 +17,7 @@ export default function Backdrop() {
                         <style>{`
                             @media (max-width: 640px) {
                                 #backdrop-image {
-                                    object-position: 90% center !important;
+                                    object-position: 88% center !important;
                                 }
                             }
                         `}</style>
