c87c9ab2c3f1600158805b1b5eb6cdf7fd917756
Mehdi Chraibi <chraibimehdi11@gmail.com>
2025-12-27 20:09:47 -0500

    Added analytics component

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366297c251df004942d07db93fa61a5006ae763e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645f5d1ad2bb3be8ab7a6a1c6f04ee20bac45608")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
